d34ea930dd5be9f088f20fd2dfd5274c6b3858b0
Nancy Soontiens <nancy.soontiens@dfo-mpo.gc.ca>
2026-03-20 12:15:35 -0230

    updating drifters

--- notebooks/drifters/cmems-NLdeployments/downloadCMEMS.py
+++ notebooks/drifters/cmems-NLdeployments/downloadCMEMS.py
@@ -12,7 +12,8 @@ import copernicusmarine
 dataset_id = 'cmems_obs-ins_glo_phy-cur_nrt_drifter_irr'
 
 drifterID_file = 'NLDrifterIDs.yaml'
-d = 'NLSummerAZMP2024' # The deployment to download. Should be documented in drifterID_file.
+d = 'StudentsonIce2025'
+#d = 'NLSummerAZMP2024' # The deployment to download. Should be documented in drifterID_file.
 #d = 'StudentsonIce2024'
 #d = 'NLFallAZMP2024'
 #d = 'all'
